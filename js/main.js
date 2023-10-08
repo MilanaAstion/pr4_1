8ea9d863071f443f692cd960344179d09311c891
MilanaAstion <130187929+MilanaAstion@users.noreply.github.com>
2023-10-08 19:15:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#loginForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = null; // Змінено значення на null для початкового стану
+
+function toggleModalAuth() {
+    modalAuth.classList.toggle("is-open-auth");
+}
+
+function authorized() {
+    console.log("Авторизован");
+
+    function logOut() {
+        login = null;
+    
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        userName.textContent = '';
+        loginInput.value = ''; // Очистити поле вводу логіну
+        checkAuth();
+    }
+
+    userName.textContent = login;
+
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener("click", logOut);
+}
+
+function notAuthorized() {
+    console.log("Не авторизован");
+
+    function logIn(event) {
+        event.preventDefault();
+        login = loginInput.value;
+        toggleModalAuth();
+        checkAuth();
+    }
+
+    buttonAuth.addEventListener("click", toggleModalAuth);
+    closeAuth.addEventListener("click", toggleModalAuth);
+    loginForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+    if (login !== null) { // Перевірка на null
+        authorized();
+    } else {
+        notAuthorized();
+    }
+}
+
+checkAuth();
